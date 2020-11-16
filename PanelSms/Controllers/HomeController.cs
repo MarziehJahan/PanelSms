3024d82fab5d728c4b134769a3f8fb2ab80e3189
marzie Jahankhani <jahankhani1994@gmail.com>
2020-11-17 00:56:04 +0330

    Add project files.

---
+++ PanelSms/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Panel.DAL;
+using PanelSms.Models;
+
+namespace PanelSms.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        PanelSimorghContext ctx;
+        PanelSimorghViewModel simorghViewModel;
+        public HomeController()
+        {
+            ctx = new PanelSimorghContext();
+            simorghViewModel = new PanelSimorghViewModel()
+            {
+                AcquaintanceTypes = ctx.Acquaintances.Select(a => new SelectListItem()
+                {
+                    Text = a.AcquaintanceDesc,
+                    Value = a.AcquaintanceDesc,
+                    Selected=true
+                }),
+                Conditions = ctx.Conditions,
+                UserPanels = ctx.users.Select(a => new SelectListItem()
+                {
+                    Text = a.UserPanelDescription,
+                    Value = a.UserPanelDescription
+                })
+            };
+        }
+        public IActionResult Index()
+        {
+            return View(simorghViewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(PanelSimorghViewModel model)
+        {
+            return View(simorghViewModel);
+        }
+    }
+}
---
+++ PanelSms/Models/PanelSimorghViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Panel.DAL;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PanelSms.Models
+{
+    public class PanelSimorghViewModel
+    {
+        [Required(ErrorMessage ="نام الزامیست")]
+        [StringLength(15,ErrorMessage ="نام حداکثر شامل 15 کاراکتر میباشد")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "نام خانوادگی الزامیست")]
+        [StringLength(20, ErrorMessage = "نام خانوادگی حداکثر شامل 20 کاراکتر میباشد")]
+        public string FName { get; set; }
+        [Required(ErrorMessage = "نام کاربری الزامیست")]
+        [StringLength(15, ErrorMessage = "نام کاربری حداکثر شامل 20 کاراکتر میباشد")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "ایمیل الزامیست")]
+        [DataType(DataType.EmailAddress,ErrorMessage ="آدرس ایمیل نامعتبر است")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "شماره همراه الزامیست")]
+        [RegularExpression(@"09 [1 2 0 3]\d{8}",ErrorMessage ="شماره همراه نامعتبر است")]
+        public int PhoneNumber { get; set; }
+        [Required(ErrorMessage = "شماره تلفن الزامیست")]
+        [RegularExpression(@"0 \d{10}", ErrorMessage = "شماره تلفن نامعتبر است")]
+        public int PhoneCall { get; set; }
+        [Required(ErrorMessage = "کد ملی الزامیست")]
+        public int NationalCode { get; set; }
+        [Required(ErrorMessage = "شماره شناسنامه الزامیست")]
+        public int BirthNo { get; set; }
+        [Required(ErrorMessage = "کد پستی الزامیست")]
+        public int PostalCode { get; set; }
+        [Required(ErrorMessage = "آدرس الزامیست")]
+        [DataType(DataType.Text)]
+        public string Address { get; set; }
+        public int UserPanelId { get; set; }
+        public int AcquaintanceId { get; set; }
+        public IEnumerable<SelectListItem> UserPanels { get; set; }
+        public IEnumerable<Condition> Conditions { get; set; }
+        public IEnumerable<SelectListItem> AcquaintanceTypes { get; set; }
+        [Required(ErrorMessage ="پذیرش شرایط جهت ثبت نام الزامیست")]
+        public bool CheckTerms { get; set; }
+    }
+}
